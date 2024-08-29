7fba34b51edd38b1f386b0caf6379a8e8dc875db
Hamim <hamimc232@gmail.com>
2024-08-28 22:34:09 -0400

    Code formatting, installed more libraries

--- app/page.js
+++ app/page.js
@@ -2,9 +2,22 @@ import { Box, Typography } from "@mui/material";
 
 export default function Home() {
   return (
-    <Box>
-      <Typography>
-        Filler text goes here
+    <Box
+      sx={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Typography
+        sx={{
+          color: "red",
+          textAlign: "center",
+          fontWeight: "bold",
+        }}
+      >
+        Filler text goes here. Test Test
       </Typography>
     </Box>
   );

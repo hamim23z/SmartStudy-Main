7f38993cf6c4d77259f359536050039db4c367c4
Hamim <hamimc232@gmail.com>
2024-08-29 21:28:38 -0400

    Added basic headers to all pages

--- app/page.js
+++ app/page.js
@@ -1,4 +1,12 @@
-import { Box, Typography } from "@mui/material";
+"use client"
+import { useEffect, useState } from "react";
+import { AppBar, Box, Container, Typography, TextField } from "@mui/material";
+
+{/*MUI ICONS*/}
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';
 
 export default function Home() {
   return (
---
+++ app/sign-up/page.js
@@ -0,0 +1,9 @@
+"use client"
+import { useEffect, useState } from "react";
+import { AppBar, Box, Container, Typography, TextField } from "@mui/material";
+
+{/*MUI ICONS*/}
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import XIcon from '@mui/icons-material/X';

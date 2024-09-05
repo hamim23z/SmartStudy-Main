e6691743f6a8bee130affde5783c1a97674aec99
Hamim <hamimc232@gmail.com>
2024-09-04 23:49:39 -0400

    Added footer to all pages

--- app/termsandcond/page.js
+++ app/termsandcond/page.js
@@ -12,6 +12,7 @@ import {
   IconButton,
   Icon,
   Menu,
+  Stack,
 } from "@mui/material";
 import { motion } from "framer-motion";
 import Link from "next/link";
@@ -282,6 +283,259 @@ export default function TermsandConditions() {
           background: "linear-gradient(270deg, #000000, #2838ae)",
         }}
       ></Box>
+
+      {/*Footer*/}
+      <Box
+        component="footer"
+        sx={{
+          height: "40vh",
+          py: 4,
+          background: "linear-gradient(-270deg, #000000, #2838ae)",
+        }}
+      >
+        <Container maxWidth="lg">
+          <Stack direction="row" spacing={4} justifyContent="space-between">
+            <Stack spacing={2}>
+              <Typography
+                variant="h6"
+                sx={{
+                  color: "white",
+                  fontFamily: "Kanit, sans-serif",
+                  fontWeight: "900",
+                }}
+              >
+                Smart Study
+              </Typography>
+              <Typography
+                variant="body1"
+                sx={{
+                  color: "white",
+                  fontFamily: "Kanit, sans-serif",
+                }}
+              >
+                Subscribe to our newsletter
+              </Typography>
+              <Stack direction="row" spacing={1}>
+                <TextField
+                  variant="outlined"
+                  size="small"
+                  placeholder="Your email"
+                  sx={{
+                    flexGrow: 1,
+                    background: "white",
+                    borderRadius: "10px",
+                    "& .MuiInputBase-input::placeholder": {
+                      color: "black",
+                      fontFamily: "Kanit, sans-serif",
+                      fontWeight: "900",
+                    },
+                    "& .MuiInputBase-input": {
+                      color: "black",
+                      fontFamily: "Kanit, sans-serif",
+                      fontWeight: "900",
+                    },
+                  }}
+                />
+                <Button variant="contained" sx={{ borderRadius: "10px" }}>
+                  Submit
+                </Button>
+              </Stack>
+              <Stack direction="row" spacing={1}>
+                <IconButton
+                  href="https://github.com/hamim23z"
+                  target="_blank"
+                  sx={{
+                    color: "white",
+                    transition: "color 0.2s ease-in-out",
+                    "&:hover": {
+                      color: "rgba(145, 83, 209, 1)",
+                    },
+                  }}
+                >
+                  <GitHubIcon />
+                </IconButton>
+                <IconButton
+                  href="https://www.linkedin.com/in/hamimc/"
+                  target="_blank"
+                  sx={{
+                    color: "white",
+                    transition: "color 0.2s ease-in-out",
+                    "&:hover": {
+                      color: "rgba(145, 83, 209, 1)",
+                    },
+                  }}
+                >
+                  <LinkedInIcon />
+                </IconButton>
+                <IconButton
+                  href="https://instagram.com"
+                  target="_blank"
+                  sx={{
+                    color: "white",
+                    transition: "color 0.2s ease-in-out",
+                    "&:hover": {
+                      color: "rgba(145, 83, 209, 1)",
+                    },
+                  }}
+                >
+                  <InstagramIcon />
+                </IconButton>
+                <IconButton
+                  href="https://x.com"
+                  target="_blank"
+                  sx={{
+                    color: "white",
+                    transition: "color 0.2s ease-in-out",
+                    "&:hover": {
+                      color: "rgba(145, 83, 209, 1)",
+                    },
+                  }}
+                >
+                  <XIcon />
+                </IconButton>
+              </Stack>
+            </Stack>
+
+            <Stack direction="row" spacing={4}>
+              <Stack spacing={1}>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: "white",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "900",
+                  }}
+                >
+                  Company
+                </Typography>
+                <Link
+                  href="/aboutus"
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "400",
+                    paddingTop: "30px",
+                  }}
+                >
+                  About Us
+                </Link>
+                <Link
+                  href="/blog"
+                  target="_blank"
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "400",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                  }}
+                >
+                  Blog
+                </Link>
+                <Link
+                  href="https://github.com/hamim23z"
+                  target="_blank"
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "400",
+                  }}
+                >
+                  GitHub
+                </Link>
+              </Stack>
+              <Stack spacing={1}>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: "white",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "900",
+                  }}
+                >
+                  References
+                </Typography>
+                <Link
+                  href="/documentation"
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "400",
+                    paddingTop: "30px",
+                  }}
+                >
+                  Documentation
+                </Link>
+                <Link
+                  href="/pricing"
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "400",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                  }}
+                >
+                  Pricing
+                </Link>
+                <Link
+                  href="/demos"
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "400",
+                  }}
+                >
+                  Demos
+                </Link>
+              </Stack>
+              <Stack spacing={1}>
+                <Typography
+                  variant="h6"
+                  sx={{
+                    color: "white",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "900",
+                  }}
+                >
+                  Legal
+                </Typography>
+                <Link
+                  href="/privacy"
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "400",
+                    paddingTop: "30px",
+                  }}
+                >
+                  Privacy
+                </Link>
+                <Link
+                  href="/termsandcond"
+                  style={{
+                    color: "white",
+                    textDecoration: "none",
+                    fontFamily: "Kanit, sans-serif",
+                    fontWeight: "400",
+                    paddingTop: "10px",
+                    paddingBottom: "10px",
+                  }}
+                >
+                  Terms and Conditions
+                </Link>
+              </Stack>
+            </Stack>
+          </Stack>
+        </Container>
+      </Box>
     </>
   );
 }

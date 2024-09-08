adedaa92466365ba59156a880edaea220dd98488
Hamim <hamimc232@gmail.com>
2024-09-07 20:22:20 -0400

    Minor tweaks

--- app/studykit/page.js
+++ app/studykit/page.js
@@ -45,7 +45,7 @@ import InstagramIcon from "@mui/icons-material/Instagram";
 import XIcon from "@mui/icons-material/X";
 import MenuIcon from "@mui/icons-material/Menu";
 
-export default function Pricing() {
+export default function StudyKit() {
   return (
     <>
       <AppBar

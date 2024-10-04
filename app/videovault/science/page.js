c39fb9ca65c847e6e6cbce2b6bc6992f0dc0d376
Hamim <hamimc232@gmail.com>
2024-10-03 23:46:49 -0400

    Test

---
+++ app/videovault/science/page.js
@@ -0,0 +1,11 @@
+"use client";
+import React from "react";
+import { Box, Typography } from "@mui/material";
+
+export default function TestPage() {
+    return (
+        <Box>
+            <Typography>Hello Hello. This is the Video Vault Test Page.</Typography>
+        </Box>
+    );
+}
